5f6ba15115b76a50542b60aae413c8cdd7209cbb
somn45 <somnsomn45@gmail.com>
2023-02-26 16:33:18 +0900

    Case 3 : 숫자와 문자열을 검사하는 방법에 대해 알아보자.
    - 숫자는 정수와 소수에 따라 검사하는 메서드가 다르다.
    - 문자열은 toMatch를 통해 정규식으로 검사한다.

--- sum.test.js
+++ sum.test.js
@@ -1,25 +1,25 @@
-/*
-- toBeNull은 null에만 일치한다.
-- toBeUndefined는 undefined에만 일치한다.
-- toBeDefined는 undefined 외의 모든 것에 일치한다.
-- toBeTruthy는 true로 취급하는 모든 것에 일치한다.
-- toBeFalsy는 false로 취급하는 모든 것에 일치한다.
-*/
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
 
-test('null', () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
+  // toBe와 toEqual은 숫자에 대해 동등합니다
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
 });
 
-test('zero', () => {
-  const z = 0;
-  expect(z).toBeNull(); // test failed
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           반올림 오류로 동작하지 않을 것입니다
+  expect(value).toBeCloseTo(0.3); // 동작합니다
+});
+
+test('team이라는 문자열에는 I가 포함되어 있지 않다.', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('Christoph라는 문자열에는 문자열 stop이 포함되어 있다.', () => {
+  expect('Christoph').toMatch(/stop/);
 });
