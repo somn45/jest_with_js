d3bde8a937e5dfc560e0153e5a95d2e98c087200
somn45 <somnsomn45@gmail.com>
2023-02-26 16:54:19 +0900

    Case 5 : 특정 함수에서 오류가 발생하는지 테스트 하는 법을 알아보자.
    - toThrow를 사용하면 특정 함수에서 오류가 발생할 때  테스트가 통과한다.
    - 문자열, 정규식을 통해 오류 메세지까지 테스트할 수 있다.

--- sum.test.js
+++ sum.test.js
@@ -1,12 +1,12 @@
-const shoppingList = [
-  'diapers',
-  'kleenex',
-  'trash bags',
-  'paper towels',
-  'beer',
-];
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK');
+}
 
-test('shoppingList 배열에 beer 아이템이 포함되어 있다.', () => {
-  expect(shoppingList).toContain('beer');
-  expect(new Set(shoppingList)).toContain('beer');
+test('compileAndroidCode 함수에서 오류가 발생하나?', () => {
+  expect(compileAndroidCode).toThrow();
+  expect(compileAndroidCode).toThrow(Error);
+
+  // 정확한 오류 메세지나 정규식을 사용할 수도 있다.
+  expect(compileAndroidCode).toThrow('you are using the wrong JDK');
+  expect(compileAndroidCode).toThrow(/JDK/);
 });
