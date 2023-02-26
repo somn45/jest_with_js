bd82171bf9815f8c462306342067dfe4b6e96469
somn45 <somnsomn45@gmail.com>
2023-02-26 16:46:47 +0900

    Case 4 : 배열이나 이터러블을 검사하는 방법에 대해 알아보자.
    - toContain을 통해 배열이나 이터러블에 특정한 아이템이 있는지 확인할 수
      있다.
    - toContain 말고도 배열을 검사하는 메서드는 매우 다양하다. 숫자나
      문자열도 마찬가지

--- sum.test.js
+++ sum.test.js
@@ -1,25 +1,12 @@
-test('two plus two', () => {
-  const value = 2 + 2;
-  expect(value).toBeGreaterThan(3);
-  expect(value).toBeGreaterThanOrEqual(3.5);
-  expect(value).toBeLessThan(5);
-  expect(value).toBeLessThanOrEqual(4.5);
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'beer',
+];
 
-  // toBe와 toEqual은 숫자에 대해 동등합니다
-  expect(value).toBe(4);
-  expect(value).toEqual(4);
-});
-
-test('adding floating point numbers', () => {
-  const value = 0.1 + 0.2;
-  //expect(value).toBe(0.3);           반올림 오류로 동작하지 않을 것입니다
-  expect(value).toBeCloseTo(0.3); // 동작합니다
-});
-
-test('team이라는 문자열에는 I가 포함되어 있지 않다.', () => {
-  expect('team').not.toMatch(/I/);
-});
-
-test('Christoph라는 문자열에는 문자열 stop이 포함되어 있다.', () => {
-  expect('Christoph').toMatch(/stop/);
+test('shoppingList 배열에 beer 아이템이 포함되어 있다.', () => {
+  expect(shoppingList).toContain('beer');
+  expect(new Set(shoppingList)).toContain('beer');
 });
